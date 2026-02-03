a706a293c50ddf91e3574ba9b7cea8da119fb9c4
Leehyeonwoo <hyeon.woo7789@gmail.com>
2026-02-03 22:11:21 +0900

    javascript loop

---
+++ js_pr/loop.js
@@ -0,0 +1,19 @@
+// 반복문
+
+// while
+let num = 1;
+while(num <= 10) {
+    console.log(num);
+    num++;
+}
+
+// do while
+do {
+    console.log("무조건");
+    console.log("한 번은 실행");
+} while(false);
+
+// for 
+for(let i = 0; i < 5; i++) {
+    console.log(i);
+}
