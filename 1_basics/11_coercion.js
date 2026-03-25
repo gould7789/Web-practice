44e45bad6f0d2952f5b1077b7da3550c3f17d4d0
Lee Hyeonwoo <hyeon.woo7789@gmail.com>
2026-03-25 22:38:58 +0900

    java script Type Conversion

---
+++ 1_basics/11_coercion.js
@@ -0,0 +1,29 @@
+/**
+ * 타입 변환
+ * Type Conversion
+ * 
+ * 1) 명시적
+ * 2) 암묵적
+ */
+
+let age = 32;
+
+// 명시적
+let stringAge = age.toString();
+console.log(typeof stringAge, stringAge);
+
+// 암묵적
+let test = age + '';
+console.log(typeof test, test);
+
+console.log('98' + 2);
+console.log('98' * 2);
+console.log('98' - 2);
+
+/**
+ * Boolean 타입으로의 변환
+ */
+console.log(!!'abcdefg');
+console.log(!!'');
+console.log(!!0);
+console.log(!!'0');
